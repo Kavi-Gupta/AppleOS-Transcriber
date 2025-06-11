b782eb0762fc886865cbce2a74bc5a03cc63c0b6
Kavi Gupta <69233899+Kavi-Gupta@users.noreply.github.com>
2025-06-11 09:20:10 -0700

    Initial Commit

---
+++ Transcriber/TranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriberApp.swift
+//  Transcriber
+//
+//  Created by Kavi Gupta on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
